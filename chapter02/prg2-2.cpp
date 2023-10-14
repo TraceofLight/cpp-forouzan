bcbf391e280e94225a03965ecb51857f57921155
TraceofLight <rickyjun96@gmail.com>
2023-10-14 23:41:10 +0900

    23-10-14

---
+++ chapter02/prg2-2.cpp
@@ -0,0 +1,21 @@
+/*
+별로 사각형을 출력하는 프로그램
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******" << endl;
+    cout << "******";
+    return 0;
+}
+
+// 블록 주석의 경우 컴파일러는 완전히 무시, 중간에 시작 기호나 종료 기호가 들어가면 오류가 발생
+// namespace: 객체의 성을 입력하지 않은 경우 std를 넣고 판단하라는 의미로 컴파일러에게 전달
